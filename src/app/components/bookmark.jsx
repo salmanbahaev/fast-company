f43a7a3b65e642a59086352b26cb32463d6876c6
Salman Bahaev <bahaev.sm@gmail.com>
2025-01-13 20:39:29 +0300

    mark edit

--- src/app/components/bookmark.jsx
+++ src/app/components/bookmark.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const BookMark = ({ bookmark }) => {
   console.log(bookmark);
   
-  const classes = bookmark ? "bi bi-bookmark" : "bi bi-bookmark-heart-fill";
+  // const classes = bookmark ? "bi bi-bookmark" : "bi bi-bookmark-heart-fill";
   return (
     <>
       <button>
